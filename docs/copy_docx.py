02e16b55468072f972533d29dfe6962c2293e65e
Antonis Loumiotis <al459@columbia.edu>
2025-05-01 16:52:41 +0300

    Improve ifdat2dat

--- docs/copy_docx.py
+++ docs/copy_docx.py
@@ -3,6 +3,6 @@ import os
 import shutil
 
 root = Path(os.path.abspath(__file__)).parent
-src = root.joinpath("_build/docx/ELFund-docs-.docx")
+src = root.joinpath("_build/html/docx/ELFund-docs.docx")
 dst = root.joinpath("source/_static/ELFund-Docs.docx")
 shutil.copyfile(src, dst)
